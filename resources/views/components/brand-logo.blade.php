a2ecd9d036185ef9f8d3b340e9a992718bbb4f5c
Salvo Bonanno <salvob.labs@gmail.com>
2024-07-04 14:06:52 +0200

    Removes hidden text

--- resources/views/components/brand-logo.blade.php
+++ resources/views/components/brand-logo.blade.php
@@ -1,7 +1,6 @@
 <a href="{{url('/')}}">
-    <span class="sr-only">{{ $brand->name }}</span>
     @if($brand->logo->format === \IVR\MultiTenancyUtils\Enums\LogoFormat::FORMAT_RASTER)
-        <img class="h-16 w-auto" src="{{ $brand->logo->imageUrl }}" alt="">
+        <img class="h-16 w-auto" src="{{ $brand->logo->imageUrl }}" alt="{{ $brand->name }} logo">
     @elseif($brand->logo->format === \IVR\MultiTenancyUtils\Enums\LogoFormat::FORMAT_VECTOR)
         <div class="w-[160px] text-white">
             {!! $brand->logo->svgMarkup !!}
