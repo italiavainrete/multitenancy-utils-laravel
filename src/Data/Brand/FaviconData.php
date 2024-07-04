fbf28fd0868c3f5edb4e7bad2e916c2a15616bcf
Salvo Bonanno <salvob.labs@gmail.com>
2024-07-04 07:46:42 +0200

    Improves render methods

--- src/Data/Brand/FaviconData.php
+++ src/Data/Brand/FaviconData.php
@@ -2,6 +2,7 @@
 
 namespace IVR\MultiTenancyUtils\Data\Brand;
 
+use IVR\MultiTenancyUtils\MultiTenancyUtils;
 use Spatie\LaravelData\Data;
 
 class FaviconData extends Data
@@ -15,33 +16,33 @@ class FaviconData extends Data
     {
     }
 
-    public function renderBrowserConfigXml(): string
+    public function renderBrowserConfigXml(BrandData $brandData): string
     {
         return '<?xml version="1.0" encoding="utf-8"?>
             <browserconfig>
                 <msapplication>
                     <tile>
-                        <square150x150logo src="'. app('tenant')->asset('mstile-150x150.png') .'"/>
+                        <square150x150logo src="'.MultiTenancyUtils::asset("/favicons/$brandData->key/mstile-150x150.png").'"/>
                         <TileColor>'. $this->ms_tile_color .'</TileColor>
                     </tile>
                 </msapplication>
             </browserconfig>';
     }
 
-    public function renderWebManifest(): array
+    public function renderWebManifest(BrandData $brandData): array
     {
         return json_decode('
             {
-                "name": "' . app('tenant')->name . '",
-                "short_name": "' . app('tenant')->name . '",
+                "name": "' . $brandData->name . '",
+                "short_name": "' . $brandData->key . '",
                 "icons": [
                     {
-                        "src": "'. app('tenant')->asset('android-chrome-192x192.png') . '",
+                        "src": "'. MultiTenancyUtils::asset("/favicons/$brandData->key/android-chrome-192x192.png") . '",
                         "sizes": "192x192",
                         "type": "image/png"
                     },
                     {
-                        "src": "'. app('tenant')->asset('android-chrome-512x512.png') . '",
+                        "src": "'. MultiTenancyUtils::asset("/favicons/$brandData->key/android-chrome-512x512.png") . '",
                         "sizes": "512x512",
                         "type": "image/png"
                     }
@@ -52,13 +53,20 @@ class FaviconData extends Data
             }', true);
     }
 
-    public function renderFaviconMetaTags(): string
+    public function renderFaviconMetaTags(BrandData $brandData): string
     {
         return '
-            <link rel="mask-icon"
-                href="'. app('tenant')->asset('safari-pinned-tab.svg') . '"
-                color="'. $this->osx_mask_icon_color . '"
-            >
+            <link rel="shortcut icon" href="'.MultiTenancyUtils::asset("/favicons/$brandData->key/favicon.ico").'">
+            <link rel="manifest" href="/site.webmanifest">
+            <link rel="apple-touch-icon" sizes="180x180" href="'.MultiTenancyUtils::asset("/favicons/$brandData->key/apple-touch-icon.png") .'">
+            <link rel="icon" type="image/png" sizes="32x32" href="'. MultiTenancyUtils::asset("/favicons/$brandData->key/favicon-32x32.png") .'">
+            <link rel="icon" type="image/png" sizes="16x16" href="'.MultiTenancyUtils::asset("/favicons/$brandData->key/favicon-16x16.png").'">
+
+            <meta name="msapplication-config" content="/browserconfig.xml">
+            <meta name="apple-mobile-web-app-title" content="'.$brandData->name.'">
+            <meta name="application-name" content="'.$brandData->name.'">
+
+            <link rel="mask-icon" href="'. MultiTenancyUtils::asset("/favicons/$brandData->key/safari-pinned-tab.svg") . '" color="'. $this->osx_mask_icon_color . '">
             <meta name="msapplication-TileColor" content="'. $this->ms_tile_color . '">
             <meta name="theme-color" content="'. $this->android_theme_color . '">
         ';
