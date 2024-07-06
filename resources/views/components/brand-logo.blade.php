c883b624d42af432ea0133945a585621ac61b346
Salvo Bonanno <salvob.labs@gmail.com>
2024-07-06 20:23:32 +0200

    Removes link from logo and simplifies layout

--- resources/views/components/brand-logo.blade.php
+++ resources/views/components/brand-logo.blade.php
@@ -1,9 +1,7 @@
-<a href="{{url('/')}}">
+<div {{ $attributes->merge(['class' => 'block']) }}>
     @if($brand->logo->format === \IVR\MultiTenancyUtils\Enums\LogoFormat::FORMAT_RASTER)
-        <img class="h-16 w-auto" src="{{ $brand->logo->imageUrl }}" alt="{{ $brand->name }} logo">
+        <img src="{{ $brand->logo->imageUrl }}" alt="{{ $brand->name }} logo">
     @elseif($brand->logo->format === \IVR\MultiTenancyUtils\Enums\LogoFormat::FORMAT_VECTOR)
-        <div class="w-[160px] text-white">
-            {!! $brand->logo->svgMarkup !!}
-        </div>
+        {!! $brand->logo->svgMarkup !!}
     @endif
-</a>
+</div>
