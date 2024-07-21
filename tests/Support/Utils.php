4c02367070863bf685fdacbf1e4b795e4c80ce0c
Salvo Bonanno <salvob.labs@gmail.com>
2024-07-21 17:13:51 +0200

    Skips setting key property

--- tests/Support/Utils.php
+++ tests/Support/Utils.php
@@ -7,7 +7,6 @@ class Utils
     static function getTestBrandData(): array
     {
         $data = json_decode(file_get_contents(__DIR__ . '/../json/brand.json'), true);
-        $data['key'] = 'italia-va-in-rete';
         return $data;
     }
 
