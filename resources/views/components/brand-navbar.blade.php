507d49552d01d7fb49dcfc89902ace67acd7ec21
Salvo Bonanno <salvob.labs@gmail.com>
2024-07-23 22:52:02 +0200

    Includes marketplace link

--- resources/views/components/brand-navbar.blade.php
+++ resources/views/components/brand-navbar.blade.php
@@ -18,6 +18,17 @@
                     </a>
                 @endforeach
 
+                @if(!empty($brand->links->marketplace))
+                    <a id="navbar:marketplace"
+                       href="{{ $brand->links->marketplace }}"
+                       class="flex space-x-2 items-center py-2 border-t-4 border-transparent hover:text-primary hover:border-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                        </svg>
+
+                        <span>Marketplace</span>
+                    </a>
+                @endif
 
             </div>
         </div>
--- tests/Features/BrandNavbarComponentTest.php
+++ tests/Features/BrandNavbarComponentTest.php
@@ -78,3 +78,51 @@ it('renders navbar in dark mode', function () {
             ->toContain($link['icon']);
     }
 });
+
+
+it('will include marketplace link if present', function () {
+    $brand = app('tenant');
+    $brand->links->marketplace = "https://marketplace.com";
+
+    config()->set('multitenancy-utils-laravel.tenant_key', Tenants::IVR_KEY);
+    Auth::login(new User);
+
+    $user = new UserData(
+        id: '1',
+        name: 'John Doe',
+        email: 'john@email.com',
+        avatar: 'https://avatars.com/jon.svg',
+        cardNumber: '123456',
+        cardBalance: '12.99'
+    );
+    $view = (new BrandNavbar($user, "dark"))->render()->render();
+
+
+    expect($view)
+        ->toContain($brand->logo_dark->imageUrl)
+        ->toContain('id="navbar:marketplace"')
+        ->toContain("https://marketplace.com");
+});
+
+it('will not include marketplace link if missing', function () {
+    $brand = app('tenant');
+    $brand->links->marketplace = null;
+
+    config()->set('multitenancy-utils-laravel.tenant_key', Tenants::IVR_KEY);
+    Auth::login(new User);
+
+    $user = new UserData(
+        id: '1',
+        name: 'John Doe',
+        email: 'john@email.com',
+        avatar: 'https://avatars.com/jon.svg',
+        cardNumber: '123456',
+        cardBalance: '12.99'
+    );
+    $view = (new BrandNavbar($user, "dark"))->render()->render();
+
+
+    expect($view)
+        ->not()->toContain('id="navbar:marketplace"')
+        ->not()->toContain("https://marketplace.com");
+});
