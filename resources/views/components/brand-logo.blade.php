e706a85cca7e3a34a93f5fc6dae503539a5e13d4
Salvo Bonanno <salvob.labs@gmail.com>
2025-09-25 09:08:08 +0200

    Increases logo height

--- resources/views/components/brand-logo.blade.php
+++ resources/views/components/brand-logo.blade.php
@@ -2,7 +2,7 @@
     @switch($mode)
         @case('dark')
             @if($brand->logo_dark->format === \IVR\MultiTenancyUtils\Enums\LogoFormat::FORMAT_RASTER)
-                <img  style="max-height: 60px" src="{{ $brand->logo_dark->imageUrl }}" alt="{{ $brand->name }} logo">
+                <img  style="max-height: 100px" src="{{ $brand->logo_dark->imageUrl }}" alt="{{ $brand->name }} logo">
             @elseif($brand->logo_dark->format === \IVR\MultiTenancyUtils\Enums\LogoFormat::FORMAT_VECTOR)
                 {!! $brand->logo_dark->svgMarkup !!}
             @endif
@@ -10,7 +10,7 @@
 
         @case('light')
             @if($brand->logo->format === \IVR\MultiTenancyUtils\Enums\LogoFormat::FORMAT_RASTER)
-                <img style="max-height: 60px" src="{{ $brand->logo->imageUrl }}" alt="{{ $brand->name }} logo">
+                <img style="max-height: 100px" src="{{ $brand->logo->imageUrl }}" alt="{{ $brand->name }} logo">
             @elseif($brand->logo->format === \IVR\MultiTenancyUtils\Enums\LogoFormat::FORMAT_VECTOR)
                 {!! $brand->logo->svgMarkup !!}
             @endif
