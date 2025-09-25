2cab7f92fcbd0bb92ff62e166e8542e47d26d322
Salvo Bonanno <salvob.labs@gmail.com>
2025-09-25 09:36:14 +0200

    Introduces size prop for logo

--- resources/views/components/brand-logo.blade.php
+++ resources/views/components/brand-logo.blade.php
@@ -1,18 +1,35 @@
+@props(['mode' => 'light', 'size' => null])
+
+@php
+    $height = match($size) {
+        'l' => '100px',
+        default => '60px',
+    };
+@endphp
+
 <div {{ $attributes->merge(['class' => 'block']) }}>
     @switch($mode)
         @case('dark')
             @if($brand->logo_dark->format === \IVR\MultiTenancyUtils\Enums\LogoFormat::FORMAT_RASTER)
-                <img  style="max-height: 100px" src="{{ $brand->logo_dark->imageUrl }}" alt="{{ $brand->name }} logo">
+                <img style="max-height: {{ $height }}" src="{{ $brand->logo_dark->imageUrl }}" alt="{{ $brand->name }} logo">
             @elseif($brand->logo_dark->format === \IVR\MultiTenancyUtils\Enums\LogoFormat::FORMAT_VECTOR)
-                {!! $brand->logo_dark->svgMarkup !!}
+                <div style="max-height: {{ $height }}; display: inline-block;">
+                    <div style="height: 100%; max-height: inherit;">
+                        {!! $brand->logo_dark->svgMarkup !!}
+                    </div>
+                </div>
             @endif
             @break
 
         @case('light')
             @if($brand->logo->format === \IVR\MultiTenancyUtils\Enums\LogoFormat::FORMAT_RASTER)
-                <img style="max-height: 100px" src="{{ $brand->logo->imageUrl }}" alt="{{ $brand->name }} logo">
+                <img style="max-height: {{ $height }}" src="{{ $brand->logo->imageUrl }}" alt="{{ $brand->name }} logo">
             @elseif($brand->logo->format === \IVR\MultiTenancyUtils\Enums\LogoFormat::FORMAT_VECTOR)
-                {!! $brand->logo->svgMarkup !!}
+                <div style="max-height: {{ $height }}; display: inline-block;">
+                    <div style="height: 100%; max-height: inherit;">
+                        {!! $brand->logo->svgMarkup !!}
+                    </div>
+                </div>
             @endif
             @break
     @endswitch
