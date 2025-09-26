33a81911321eec2d08f0d544d946dd023558b7e8
Salvo Bonanno <salvob.labs@gmail.com>
2025-09-26 12:13:50 +0200

    Declares optional parameter at the end of the signature

--- src/Data/Brand/CompanyData.php
+++ src/Data/Brand/CompanyData.php
@@ -9,14 +9,14 @@ class CompanyData extends Data
 
     public function __construct(
         public string           $name,
-        public ?string          $tagline = null,
         public LogoData         $logo,
         public string           $vat_id,
         public string           $address,
         public string           $zip_code,
         public string           $city,
         public string           $province,
-        public CompanyLinksData $links
+        public CompanyLinksData $links,
+        public ?string          $tagline = null,
     )
     {
     }
